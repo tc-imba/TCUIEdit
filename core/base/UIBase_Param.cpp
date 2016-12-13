1922f7ddebd5a191ab9937fb6bc1263275d4118a
Liu Yihao <liuyh615@126.com>
2016-12-13 23:40:28 +0800

    fix

---
+++ core/base/UIBase_Param.cpp
@@ -0,0 +1,5 @@
+//
+// Created by liu on 2016/12/13.
+//
+
+#include "UIBase_Param.h"
---
+++ core/base/UIBase_Param.h
@@ -0,0 +1,15 @@
+//
+// Created by liu on 2016/12/13.
+//
+
+#ifndef TCUIEDIT_UIBASE_PARAM_H
+#define TCUIEDIT_UIBASE_PARAM_H
+
+
+class UIBase_Param
+{
+
+};
+
+
+#endif //TCUIEDIT_UIBASE_PARAM_H
