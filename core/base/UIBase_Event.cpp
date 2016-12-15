c9776c0a6b2467753dd7ec2f17e61befdc8d240a
Liu Yihao <liuyh615@126.com>
2016-12-15 21:31:56 +0800

    event

---
+++ core/base/UIBase_Event.cpp
@@ -0,0 +1,5 @@
+//
+// Created by liu on 2016/12/15.
+//
+
+#include "UIBase_Event.h"
---
+++ core/base/UIBase_Event.h
@@ -0,0 +1,15 @@
+//
+// Created by liu on 2016/12/15.
+//
+
+#ifndef TCUIEDIT_UIBASE_EVENT_H
+#define TCUIEDIT_UIBASE_EVENT_H
+
+
+class UIBase_Event
+{
+
+};
+
+
+#endif //TCUIEDIT_UIBASE_EVENT_H
